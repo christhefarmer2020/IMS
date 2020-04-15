a6886ee65d2e2959fbb13a14ebc13b94c791907b
John Broughton <john.broughton@lander.edu>
2020-04-15 16:20:43 -0400

    Validations

--- IMS/ViewModels/CreateVM.cs
+++ IMS/ViewModels/CreateVM.cs
@@ -8,15 +8,22 @@ namespace IMS.ViewModels
 {
     public class CreateVM
     {
+        [Required]
+        [RegularExpression(@"[123456789]{9}$", ErrorMessage = "Must be 9 digits with no special characters.")]
         public string PAT_MRN_NUM { get; set; }
 
         [Display(Name = "Appointment Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Required]
+
         public DateTime Appointment_Time { get; set; }
 
+        [Required]
         public string Consent { get; set; }
-
-        public List<HttpPostedFileBase> Images { get; set; }  
+        
+        [FileExtensions(Extensions = "JPEG,JPG,PNG,GIF,TIFF")]
+        [Required]
+        public List<HttpPostedFileBase> Images { get; set;}  
     }
 }
